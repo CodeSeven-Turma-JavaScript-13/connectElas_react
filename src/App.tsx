7ec09af4fb300a56d38cc11c4e7ca1ef0cf25306
Lary <larissa.mendy@gmail.com>
2026-04-23 18:22:44 -0300

    ➖ Removing: Retirando rota cadastro 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
-import Cadastro2 from './pages/cadastro/Cadastro2'
 import Perfil from './pages/perfil/Perfil'
 
 import FormCandidatura from './components/oportunidades/formcandidatura/FormCandidatura'
